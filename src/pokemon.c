ca391b0fb8cbb96f3e06d67bcb9593238fa8bf0d
Jacob Weisbeck <weisbeckjacob@gmail.com>
2024-08-22 23:44:03 -0400

    Removing mistake about mystery type

--- src/pokemon.c
+++ src/pokemon.c
@@ -2556,7 +2556,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
             damage = 1;
     }
 
-    if (IS_MOVE_STATUS(gCurrentMove))
+    if (type == TYPE_MYSTERY)
         damage = 0; // is ??? type. does 0 damage.
 
     if (IS_MOVE_SPECIAL(gCurrentMove))
